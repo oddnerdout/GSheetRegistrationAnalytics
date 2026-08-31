77bc4bbcdc9c74784be3b7b93fe539183f2ebfdf
oddnerdout <jtychang@gmail.com>
2026-08-31 14:20:54 +0000

    new edit

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 # Securely retrieve Sheet ID from Environment Variables
 # Set a default fallback for local development if needed
-SHEET_ID = os.environ.get("SHEET_ID", "")
+NY")
 
 def fetch_sheet_data():
     """Downloads and parses the Google Sheet from the hidden SHEET_ID."""
